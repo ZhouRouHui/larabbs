52c9a980fcaa42c8a8fa030895ab14f309536a5f
Loedan <loedan.zhou@hwua.com>
2018-05-10 11:02:33 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,23 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    //
+    /**
+     * Laravel 中间件 (Middleware) 为我们提供了一种非常棒的过滤机制来过滤进入应用的 HTTP 请求，
+     * 例如，当我们使用 Auth 中间件来验证用户的身份时，如果用户未通过身份验证，则 Auth 中间件会把用户重定向到登录页面。
+     * 如果用户通过了身份验证，则 Auth 中间件会通过此请求并接着往下执行。Laravel 框架默认为我们内置了一些中间件，
+     * 例如身份验证、CSRF 保护等。所有的中间件文件都被放在项目的 app/Http/Middleware 文件夹中。
+     * 下面使用 Laravel 提供身份验证（Auth）中间件来过滤未登录用户的 edit, update 动作
+     * __construct 是 PHP 的构造器方法，当一个类对象被创建之前该方法将会被调用。
+     * 我们在 __construct 方法中调用了 middleware 方法，该方法接收两个参数，第一个为中间件的名称，第二个为要进行过滤的动作。
+     * 我们通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤，意为 —— 除了此处指定的动作以外，
+     * 所有其他动作都必须登录用户才能访问，类似于黑名单的过滤机制。相反的还有 only 白名单方法，将只过滤指定动作。
+     * 我们提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的，此为最佳实践。
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,11 +33,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+    	$this->authorize('update', $user);
     	$data = $request->all();
 
         if ($request->avatar) {

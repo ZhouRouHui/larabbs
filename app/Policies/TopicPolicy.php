3b67961215c941d2772677ada724e606e1484eaf
Loedan <loedan.zhou@hwua.com>
2018-05-10 16:26:21 +0800

    Users can edit their own topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,8 +9,8 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        return $topic->user_id == $user->id;	// 限制只有作者自己才能对自己创建的话题进行编辑
+        // return true;
     }
 
     public function destroy(User $user, Topic $topic)

d35b4202c5dca9629c238e83c72f37a2cbd4d760
Loedan <loedan.zhou@hwua.com>
2018-05-10 14:31:10 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 一对一关系，有了这样的关联设定，后面开发中我们可以很方便地通过 $topic->category、$topic->user 来获取到话题对应的分类和作者。
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+	// 一对一关系
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

e41bb0a3eb27aa3a7d584ba3dfa2492375a394e7
Loedan <loedan.zhou@hwua.com>
2018-05-12 18:47:09 +0800

    Admin can manage users

--- config/administrator.php
+++ config/administrator.php
@@ -77,7 +77,12 @@ return array(
      *      'Analytics' => ['E-Commerce' => 'page.pages.analytics'],
      *  ]
      */
-    'menu' => [],
+    // 'menu' => [],
+    'menu' => [
+        '用户与权限' => [
+            'users',
+        ],
+    ],
 
     /*
      * The permission option is the highest-level authentication check that lets you define a closure that should return true if the current user

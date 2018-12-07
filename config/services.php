1f6667d5119a45aa884b0f34c1676c1fedc66107
Loedan <loedan.zhou@hwua.com>
2018-12-07 16:47:18 +0800

    配置百度翻译api接口

--- config/services.php
+++ config/services.php
@@ -35,6 +35,11 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID'),
+        'key'   => env('BAIDU_TRANSLATE_KEY'),
+    ],
+
     'weixin' => [
         'client_id' => env('WEIXIN_KEY'),
         'client_secret' => env('WEIXIN_SECRET'),
